22ed8e47b42fb4ac16243e5fd37998c365164682
Daidou <dylanhauh@gmail.com>
2017-10-19 09:21:32 +0200

    code

--- php/Main.php
+++ php/Main.php
@@ -4,7 +4,10 @@ require_once "Personne.php";
 
 
 $Personne1 = new Personne(1,"dylan","haubourdin","30/08/1996","M","Celibataire");
+$Personne2 = new Personne(2,"ludo","Gillodes","23/05/1996","M","Celibataire");
 
 $Personne1->getPersonne();
+$Personne2->getPersonne();
 
 var_dump($Personne1->getPersonne());
+var_dump($Personne2->getPersonne());
