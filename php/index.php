238eaaceb30b14df9bab8ed43f87e5fa9df43a6f
Daidou <dylanhauh@gmail.com>
2017-10-19 10:21:51 +0200

    la page html et css

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link type="text/css" href="css/MainStyle.css">
+    <title>Accueil</title>
+  </head>
+  <body>
+    <ul id="nav">
+        <li><a href="#">Marriage</a></li>
+        <li><a href="#">Divorce</a></li>
+        <li><a href="#">Etat de Personne</a></li>
+        <li><a href="#">affichage des Personne</a></li>
+        <li><a href="#">Saisie des Personnes</a></li>
+    </ul>
+  </body>
+</html>
